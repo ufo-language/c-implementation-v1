b6705066e0be0568c58b1889b7194cce8ffa1c8d
Jeff Meunier <jeffrey.a.meunier@gmail.com>
2021-05-08 07:33:47 -0400

    Cleanup.

--- src/main.c
+++ src/main.c
@@ -4,17 +4,6 @@
 #include "mem.h"
 #include "repl.h"
 
-#if 0
-void showStats() {
-  printf("System stats:\n");
-  printf("Size of char:  %ld bits\n", sizeof(char) * 8);
-  printf("Size of int:   %ld bits\n", sizeof(int) * 8);
-  printf("Size of long:  %ld bits\n", sizeof(long) * 8);
-  printf("Size of void*: %ld bits\n", sizeof(void*) * 8);
-  printf("Size of uint:  %ld bits\n", sizeof(uint) * 8);
-}
-#endif
-
 int main(int argc, char** argv) {
   (void)argc;  /* suppresses unused parameter warning */
   (void)argv;  /* suppresses unused parameter warning */
