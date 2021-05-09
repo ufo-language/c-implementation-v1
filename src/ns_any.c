e9d6aa560aa15bb71494b58e64286d30e2300d0e
Jeff Meunier <jeffrey.a.meunier@gmail.com>
2021-05-09 07:45:06 -0400

    Added any:freeVars and any:hashCode functions.

--- src/ns_any.c
+++ src/ns_any.c
@@ -2,8 +2,10 @@
 
 #include "d_binding.h"
 #include "d_hash.h"
+#include "d_int.h"
 #include "d_list.h"
 #include "d_prim.h"
+#include "d_set.h"
 #include "d_symbol.h"
 #include "delegate.h"
 #include "e_ident.h"
@@ -11,33 +13,58 @@
 #include "namespace.h"
 #include "object.h"
 
+Object any_freeVars(Thread* thd, Object args);
+Object any_hashCode(Thread* thd, Object args);
 Object any_match(Thread* thd, Object args);
 Object any_type(Thread* thd, Object args);
 
-static Object any_match_paramTypes;
-static Object any_type_paramTypes;
+static Object any_oneParam;
+static Object any_twoParams;
 
+/*------------------------------------------------------------------*/
 Object any_defineAll(Object env) {
+  any_oneParam = listNew(symbolNew("Any"), EMPTY_LIST);
+  any_twoParams = listNew(symbolNew("Any"), listNew(symbolNew("Any"), EMPTY_LIST));
+
   Object ns = hashNew();
-
+  nsAddPrim(ns, "freeVars", any_freeVars);
+  nsAddPrim(ns, "hashCode", any_hashCode);
   nsAddPrim(ns, "match", any_match);
-  any_match_paramTypes = listNew(symbolNew("Any"), listNew(symbolNew("Any"), EMPTY_LIST));
   nsAddPrim(ns, "type", any_type);
-  any_type_paramTypes = listNew(identNew("Any"), EMPTY_LIST);
-
   Object binding = bindingNew(identNew("any"), ns);
   return listNew(binding, env);
 }
 
+/*------------------------------------------------------------------*/
+Object any_freeVars(Thread* thd, Object args) {
+  (void)thd;
+  primCheckArgs(any_oneParam, args, thd);
+  Object freeVarSet = setNew();
+  Object arg = listGetFirst(args);
+  objFreeVars(arg, freeVarSet);
+  return freeVarSet;
+}
+
+/*------------------------------------------------------------------*/
+Object any_hashCode(Thread* thd, Object args) {
+  (void)thd;
+  primCheckArgs(any_oneParam, args, thd);
+  Object arg = listGetFirst(args);
+  Word hashCode = objHashCode(arg);
+  return intNew(hashCode);
+}
+
+/*------------------------------------------------------------------*/
 Object any_match(Thread* thd, Object args) {
   (void)thd;
-  primCheckArgs(any_match_paramTypes, args, thd);
+  primCheckArgs(any_twoParams, args, thd);
   return objMatch(listGetFirst(args), listGetSecond(args), EMPTY_LIST);
 }
 
+/*------------------------------------------------------------------*/
 Object any_type(Thread* thd, Object args) {
   (void)thd;
-  primCheckArgs(any_type_paramTypes, args, thd);
+  primCheckArgs(any_oneParam, args, thd);
   Object arg = listGetFirst(args);
   ObjType objType = objGetType(arg);
   char* typeName = ObjTypeNames[objType];
