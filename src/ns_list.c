37bade8b4f015efc3428a4a2ae1ba7f31cea06b1
Jeff Meunier <jeffrey.a.meunier@gmail.com>
2021-05-10 21:08:53 -0400

    Added list:map function.

--- src/ns_list.c
+++ src/ns_list.c
@@ -9,7 +9,9 @@
 #include "d_set.h"
 #include "d_symbol.h"
 #include "delegate.h"
+#include "e_app.h"
 #include "e_ident.h"
+#include "eval.h"
 #include "globals.h"
 #include "namespace.h"
 #include "object.h"
@@ -18,22 +20,26 @@ Object list_count(Thread* thd, Object args);
 Object list_drop(Thread* thd, Object args);
 Object list_first(Thread* thd, Object args);
 Object list_keys(Thread* thd, Object args);
+Object list_map(Thread* thd, Object args);
 Object list_rest(Thread* thd, Object args);
 Object list_reverse(Thread* thd, Object args);
 Object list_take(Thread* thd, Object args);
 
 static Object param_List;
+static Object param_ListAny;
 static Object param_ListInt;
 
 /*------------------------------------------------------------------*/
 Object list_defineAll(Object env) {
   param_List = primBuildTypeList(1, D_List);
+  param_ListAny = primBuildTypeList(2, D_List, D_Null);
   param_ListInt = primBuildTypeList(2, D_List, D_Int);
   Object ns = hashNew();
   nsAddPrim(ns, "drop", list_drop);
   nsAddPrim(ns, "count", list_count);
   nsAddPrim(ns, "first", list_first);
   nsAddPrim(ns, "keys", list_keys);
+  nsAddPrim(ns, "map", list_map);
   nsAddPrim(ns, "rest", list_rest);
   nsAddPrim(ns, "reverse", list_reverse);
   nsAddPrim(ns, "take", list_take);
@@ -86,6 +92,26 @@ Object list_keys(Thread* thd, Object args) {
   Object keySet = setNew();
   listEach(list, list_keys_callback, keySet);
   return keySet;
+}
+
+/*------------------------------------------------------------------*/
+Object list_map(Thread* thd, Object args) {
+  primCheckArgs(param_ListAny, args, thd);
+  Object list = listGetFirst(args);
+  Object fun = listGetSecond(args);
+  Object funVal = eval(fun, thd);
+  Object q = queueNew();
+  while (!listIsEmpty(list)) {
+    Object elem = listGetFirst(list);
+    Object app = appNew(funVal, listNew(elem, EMPTY_LIST));
+    Object val = eval(app, thd);
+    queueEnq(q, val);
+    list = listGetRest(list);
+    if (objGetType(list) != D_List) {
+      list = listNew(list, EMPTY_LIST);
+    }
+  }
+  return queueAsList(q);
 }
 
 /*------------------------------------------------------------------*/
